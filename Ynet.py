1091dad4b96a37f4992ea2ec0bb35d0d96971650
24x6 <Dorit.yacobi@gmail.com>
2024-07-03 04:15:07 +0300

    Update Ynet.py

--- Ynet.py
+++ Ynet.py
@@ -9,37 +9,14 @@ class TelegramForwarder:
         self.phone_number = phone_number
         self.client = TelegramClient('session_' + phone_number, api_id, api_hash)
 
-    async def list_chats(self):
-        await self.client.connect()
-
-        # Ensure you're authorized
-        #if not await self.client.is_user_authorized():
-         #   await self.client.send_code_request(self.phone_number)
-          #  await self.client.sign_in(self.phone_number, input('Enter the code: '))
-
-        # Get a list of all the dialogs (chats)
-        dialogs = await self.client.get_dialogs()
-        chats_file = open(f"chats_of_{self.phone_number}.txt", "w")
-        # Print information about each chat
-        for dialog in dialogs:
-            print(f"Chat ID: {dialog.id}, Title: {dialog.title}")
-            chats_file.write(f"Chat ID: {dialog.id}, Title: {dialog.title} \n")
-          
-
-        print("List of groups printed successfully!")
 
     async def forward_messages_to_channel(self, source_chat_id, destination_channel_id, keywords):
         await self.client.connect()
-
-        # Ensure you're authorized
-        #if not await self.client.is_user_authorized():
-         #   await self.client.send_code_request(self.phone_number)
-          #  await self.client.sign_in(self.phone_number, input('Enter the code: '))
-
+        
         last_message_id = (await self.client.get_messages(source_chat_id, limit=1))[0].id
 
         while True:
-
+            print("Checking for messages and forwarding them...")
             # Get new messages since the last checked message
             messages = await self.client.get_messages(source_chat_id, min_id=last_message_id, limit=None)
 
@@ -75,31 +52,18 @@ def read_credentials():
         print("Credentials file not found.")
         return None, None, None
 
-# Function to write credentials to file
-def write_credentials(api_id, api_hash, phone_number):
-    with open("credentials.txt", "w") as file:
-        file.write(api_id + "\n")
-        file.write(api_hash + "\n")
-        file.write(phone_number + "\n")
 
 async def main():
     print('bot start')
     # Attempt to read credentials from file
     api_id, api_hash, phone_number = read_credentials()
-
-    # If credentials not found in file, prompt the user to input them
-    if api_id is None or api_hash is None or phone_number is None:
-        api_id = 22701033 #input("Enter your API ID: ")
-        api_hash = '9428ef925607e9afe220e31176170b70' # input("Enter your API Hash: ")
-        phone_number = +972504640969 #input("Enter your phone number: ")
-        # Write credentials to file for future use
-        write_credentials(api_id, api_hash, phone_number)
+    print('read_credentials')
 
     forwarder = TelegramForwarder(api_id, api_hash, phone_number)
 
-    source_chat_id = -1001397114707 #YNET #int(input("Enter the source chat ID: "))
-    destination_channel_id = -1001745841781 #24*6 #int(input("Enter the destination chat ID: "))
-    keywords = '' #input("Put keywords (comma separated if multiple, or leave blank): ").split(",")
+    source_chat_id = -1001397114707 
+    destination_channel_id = -1001745841781 
+    keywords = '' 
         
     await forwarder.forward_messages_to_channel(source_chat_id, destination_channel_id, keywords)
 
